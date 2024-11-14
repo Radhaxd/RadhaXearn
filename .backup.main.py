e73f28921e8b23cfa72438a90bea15efc5b1f6a8
Robocoders <team@blackbox.ai>
2024-11-14 07:42:18 +0000

    Update

--- .backup.main.py
+++ .backup.main.py
@@ -1,13 +1,10 @@
 
-from pyrogram import Client
+from pyrogram import Client, filters
 from config import API_ID, API_HASH, BOT_TOKEN
 from handlers import register_handlers
-from admin import register_admin_handlers
 
 app = Client("refer_earn_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 if __name__ == "__main__":
     register_handlers(app)
-    register_admin_handlers(app)
-    print("Bot is starting...")
     app.run()
